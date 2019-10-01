30c77630eff1eec4907352977706c316ad90e62e
Deyanira Taupier <deyanira.taupier@sap.com>
2019-09-30 23:11:21 -0700

    wipe

---
+++ app/src/main/java/com/example/deya/projectilevomcom/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.deya.projectilevomcom;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
